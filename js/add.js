11d197292647ffb08580b42ae6803f30371f12a7
ikramiiss <ikramhossian22@gmail.com>
2025-10-06 11:58:42 +0600

    all thing upload without dom

---
+++ js/add.js
@@ -0,0 +1,15 @@
+// add money for Noakhali
+
+ 
+    // Add scroll effect
+    window.addEventListener("scroll", function() {
+      const header = document.querySelector("header");
+      if (window.scrollY > 10) {
+        header.classList.add("backdrop-blur-md",  "shadow-md");
+      } else {
+        header.classList.remove("backdrop-blur-md",  "shadow-md");
+      }
+
+
+    });
+ 
